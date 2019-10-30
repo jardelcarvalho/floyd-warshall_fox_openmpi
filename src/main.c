68abb311d05adf58ef6fb8e32d812d363eb7b7fe
jardelcarvalho <jardelfelipe.carvalho@gmail.com>
2019-10-30 01:33:14 -0300

    deadlock corrigido

--- src/main.c
+++ src/main.c
@@ -30,9 +30,10 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     int q = sqrt(num_procs);
+    int N, t;
+    float *my_sm;
 
     if(my_rank == P_MASTER_RANK) {
-        int N;
         float *m = le_matriz(argv[1], &N);
         if(!m) {
             MPI_Finalize();
@@ -54,10 +55,13 @@ int main(int argc, char **argv) {
         // inicialização Floyd-Warshall
         inicia_matriz_fl_war(m, N);
 
+        // master pegando sua submatriz
+        my_sm = submatriz(P_MASTER_RANK, N, q, m);
+
         // envia submatrizes para os demais processos
-        int t = N / q;
+        t = N / q;
         t *= t;
-        for(int p = 0; p < num_procs; p++) {
+        for(int p = 1; p < num_procs; p++) {
             float *sm = submatriz(p, N, q, m);
             if(!sm) {
                 free(m);
@@ -82,19 +86,20 @@ int main(int argc, char **argv) {
     // com exceção dos condicionais para P_MASTER_RANK
 
     // recebe dados enviados pelo master
-    int N;
-    MPI_Recv(&N, 1, MPI_INT, P_MASTER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    if(my_rank != P_MASTER_RANK) {
+        MPI_Recv(&N, 1, MPI_INT, P_MASTER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    int t = N / q;
-    t *= t;
-    float *my_sm = malloc(t * sizeof(float));
-    if(!my_sm) {
-        printf("erro: problema ao receber submatriz");
-        free(my_sm);
-        MPI_Finalize();
-        exit(EXIT_FAILURE);
+        t = N / q;
+        t *= t;
+        my_sm = malloc(t * sizeof(float));
+        if(!my_sm) {
+            printf("erro: problema ao receber submatriz");
+            free(my_sm);
+            MPI_Finalize();
+            exit(EXIT_FAILURE);
+        }
+        MPI_Recv(my_sm, t, MPI_FLOAT, P_MASTER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-    MPI_Recv(my_sm, t, MPI_FLOAT, P_MASTER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     // tamanho de uma dimensão da submatriz
     int sm_dim = sqrt(t);
@@ -102,28 +107,45 @@ int main(int argc, char **argv) {
     // aloca matrizes auxiliares
     float *m_a = malloc(t * sizeof(float));
     float *m_b = malloc(t * sizeof(float));
+    float *m_b_cpy = malloc(t * sizeof(float));
     float *m_res = malloc(t * sizeof(float));
     if(!m_a) {
         printf("erro: problema ao alocar matriz a\n");
-        free(my_sm);
+        free(m_a);
         free(m_b);
+        free(m_b_cpy);
         free(m_res);
+        free(my_sm);
         MPI_Finalize();
         exit(EXIT_FAILURE);
     }
     if(!m_b) {
         printf("erro: problema ao alocar mat b\n");
-        free(my_sm);
-        free(m_res);
         free(m_a);
+        free(m_b);
+        free(m_b_cpy);
+        free(m_res);
+        free(my_sm);
+        MPI_Finalize();
+        exit(EXIT_FAILURE);
+    }
+    if(!m_b_cpy) {
+        printf("erro: problema ao alocar matriz resultado\n");
+        free(m_a);
+        free(m_b);
+        free(m_b_cpy);
+        free(m_res);
+        free(my_sm);
         MPI_Finalize();
         exit(EXIT_FAILURE);
     }
     if(!m_res) {
         printf("erro: problema ao alocar matriz resultado\n");
-        free(my_sm);
-        free(m_b);
         free(m_a);
+        free(m_b);
+        free(m_b_cpy);
+        free(m_res);
+        free(my_sm);
         MPI_Finalize();
         exit(EXIT_FAILURE);
     }
@@ -133,6 +155,8 @@ int main(int argc, char **argv) {
     int u, r = my_rank / q;
     int rank_acima = (my_rank + q * (q - 1)) % num_procs;
     int rank_abaixo = (my_rank + q) % num_procs;
+    MPI_Request request;
+    MPI_Status status;
 
     for(int f = 1; f < N; f *= 2) {
 
@@ -144,23 +168,33 @@ int main(int argc, char **argv) {
             u = (r + step) % q;
 
             if(u == my_rank % q) {
+                memcpy(m_a, my_sm, t * sizeof(float));
+
                 // enviando submatriz escolhida para demais processos da linha
                 for(int sp = q * r; sp < q * r + q; sp++) {
-                    MPI_Send(my_sm, t, MPI_FLOAT, sp, 0, MPI_COMM_WORLD);
+                    if(sp != my_rank) {
+                        MPI_Send(my_sm, t, MPI_FLOAT, sp, 0, MPI_COMM_WORLD);
+                    }
                 }
+            } else {
+                // recebendo submatriz u da sua linha
+                MPI_Recv(m_a, t, MPI_FLOAT, r * q + u, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             }
-
-            // recebendo submatriz u da sua linha
-            MPI_Recv(m_a, t, MPI_FLOAT, r * q + u, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
             // Floyd-Warshall
             floyd_warshall_mmult(m_a, m_b , m_res, sm_dim);
 
-            // envia própria matriz para processo da linha de cima
-            MPI_Send(m_b, t, MPI_FLOAT, rank_acima, 0, MPI_COMM_WORLD);
+            // envia própria matriz para processo da linha de cima, não bloqueante
+            MPI_Isend(m_b, t, MPI_FLOAT, rank_acima, 0, MPI_COMM_WORLD, &request);
+
+            // copia m_b para m_b_cpy para caso a chamada mpi wait necessite reenviar m_b
+            memcpy(m_b_cpy, m_b, t * sizeof(float));
 
             // recebe matriz do processo da linha de baixo
             MPI_Recv(m_b, t, MPI_FLOAT, rank_abaixo, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // aguarda até que a linha de cima tenha recebido a matriz
+            MPI_Wait(&request, &status);
         }
 
         for(int i = 0; i < t; i++) {
@@ -168,11 +202,11 @@ int main(int argc, char **argv) {
         }
     }
 
-    // envia submatriz para o master
-    MPI_Send(m_res, t, MPI_FLOAT, P_MASTER_RANK, 0, MPI_COMM_WORLD);
-
-    // master recebe e agrupa submatrizes
-    if(my_rank == P_MASTER_RANK) {
+    if(my_rank != P_MASTER_RANK) {
+        // envia submatriz para o master
+        MPI_Send(m_res, t, MPI_FLOAT, P_MASTER_RANK, 0, MPI_COMM_WORLD);
+    } else {
+        // master recebe e agrupa submatrizes
         float *m = malloc(N * N * sizeof(float));
         if(!m) {
             printf("erro: problema ao agrupar as submatrizes\n");
@@ -183,7 +217,11 @@ int main(int argc, char **argv) {
             MPI_Finalize();
             exit(EXIT_FAILURE);
         }
-        for(int p = 0; p < num_procs; p++) {
+
+        // master encaixando a sua submatriz
+        encaixa(m_res, m, N, q, P_MASTER_RANK);
+
+        for(int p = 1; p < num_procs; p++) {
             MPI_Recv(m_res, t, MPI_FLOAT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             encaixa(m_res, m, N, q, p);
         }
@@ -195,7 +233,7 @@ int main(int argc, char **argv) {
         print_m(m, N);
         printf("\n");
 
-	printf("t = %.15lfs\n", t_fim - t_ini);	
+        printf("t = %.15lfs\n", t_fim - t_ini);	
 
         free(m);
     }
